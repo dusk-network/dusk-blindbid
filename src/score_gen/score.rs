9e17919ee0d8aac3a43b1c5e520afda7db114785
CPerezz <carlos@dusk.network>
2020-06-18 18:20:48 +0200

    Adress merge comments from @zerO in #10
    It was suggested to use `fold` since we had an accumulator in
    the bit iterator in the `single_complex_range_proof_gadget` function.
    
    - Removed `for_each` to use `fold`.

--- src/score_gen/score.rs
+++ src/score_gen/score.rs
@@ -278,23 +278,25 @@ fn single_complex_range_proof(
     let bits = scalar_to_bits(&(witness + b_prime));
 
     let mut var_accumulator = composer.zero_var;
-    let mut accumulator = Scalar::zero();
 
-    bits[..129].iter().enumerate().for_each(|(idx, bit)| {
-        let bit_var = composer.add_input(Scalar::from(*bit as u64));
-        // Apply boolean constraint to the bit.
-        composer.bool_gate(bit_var);
-        // Accumulate the bit multiplied by 2^(i-1) as a variable
-        var_accumulator = composer.add(
-            (Scalar::one(), var_accumulator),
-            (Scalar::from(2u64).pow(&[idx as u64, 0, 0, 0]), bit_var),
-            Scalar::zero(),
-            Scalar::zero(),
-        );
-        // Compute the same accumulator with scalars
-        accumulator = accumulator
-            + (Scalar::from(*bit as u64) * Scalar::from(2u64).pow(&[idx as u64, 0, 0, 0]));
-    });
+    let accumulator =
+        bits[..129]
+            .iter()
+            .enumerate()
+            .fold(Scalar::zero(), |scalar_accum, (idx, bit)| {
+                let bit_var = composer.add_input(Scalar::from(*bit as u64));
+                // Apply boolean constraint to the bit.
+                composer.bool_gate(bit_var);
+                // Accumulate the bit multiplied by 2^(i-1) as a variable
+                var_accumulator = composer.add(
+                    (Scalar::one(), var_accumulator),
+                    (Scalar::from(2u64).pow(&[idx as u64, 0, 0, 0]), bit_var),
+                    Scalar::zero(),
+                    Scalar::zero(),
+                );
+                scalar_accum
+                    + (Scalar::from(*bit as u64) * Scalar::from(2u64).pow(&[idx as u64, 0, 0, 0]))
+            });
     // Compute `Chi(x)` =  Sum(vi * 2^(i-1)) - (x + b').
     let witness_plus_b_prime = composer.add_input(witness + b_prime);
     // Note that the result will be equal to: `0 (if the reangeproof holds)
