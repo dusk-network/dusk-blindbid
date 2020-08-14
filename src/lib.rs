35e780a207e525a8c26858157e89c040af9a28b2
CPerezz <carlos@dusk.network>
2020-08-14 10:15:29 +0200

    Make V_MIN and V_MAX &'static JubJubScalar

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,10 @@ pub mod bid;
 pub mod proof;
 pub mod score_gen;
 
+use dusk_plonk::jubjub::Fr as JubJubScalar;
 /// The minimum amount user is permitted to bid
-pub const V_MIN: u64 = 50_000;
+pub const V_MIN: &'static JubJubScalar =
+    &JubJubScalar::from_raw([50_000u64, 0, 0, 0]);
 /// The maximum amount user is permitted to bid
-pub const V_MAX: u64 = 250_000;
+pub const V_MAX: &'static JubJubScalar =
+    &JubJubScalar::from_raw([250_000u64, 0, 0, 0]);
