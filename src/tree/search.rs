f895ab4865c351f0282fd51224af37c8c9e796d5
Victor Lopez <victor@dusk.network>
2020-10-01 22:37:33 +0200

    License headers on tree mod

--- src/tree/search.rs
+++ src/tree/search.rs
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
 use super::{BidTreeInner, StorageScalar};
 use kelvin::{Blake2b, Compound, HandleType, Method, SearchResult};
 
